f4661ce38936430f6f4f5eddae988617334b1b95
Vince Enachescu <vincent@enachescu.com>
2024-03-14 01:27:06 -0700

    adding lms platform

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -39,7 +39,8 @@ export class LmsPlatformAccessory {
       .setCharacteristic(this.platform.Characteristic.SerialNumber, accessory.context.device.player_id);
 
     // this.service = this.accessory.getService(this.platform.Service.Speaker) || this.accessory.addService(this.platform.Service.Speaker);
-    this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
+    // this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
+    this.service = this.accessory.getService(this.platform.Service.Switch) || this.accessory.addService(this.platform.Service.Switch);
 
     this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.player_name);
 
@@ -47,9 +48,9 @@ export class LmsPlatformAccessory {
       .onSet(this.setOn.bind(this))
       .onGet(this.getOn.bind(this));
 
-    this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-      .onSet(this.setBrightness.bind(this))
-      .onGet(this.getBrightness.bind(this));
+    // this.service.getCharacteristic(this.platform.Characteristic.Brightness)
+    //   .onSet(this.setBrightness.bind(this))
+    //   .onGet(this.getBrightness.bind(this));
 
     // this.service.getCharacteristic(this.platform.Characteristic.Volume)
     //   .onSet(this.setVolume.bind(this))
