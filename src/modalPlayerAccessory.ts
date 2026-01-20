ee7dae2edf4976fa6e3b074587dd82472f52e394
Vince Enachescu <vincent@enachescu.com>
2026-01-20 00:57:59 -0800

    trying to add input source

--- src/modalPlayerAccessory.ts
+++ src/modalPlayerAccessory.ts
@@ -11,17 +11,18 @@ import { SlimServer } from './lms';
  */
 export class LmsModalPlayerAccessory {
 
-  private inputService: Service;
-  private speakerService: Service;
+  private inputServices: Service[] = [];
+  private switchService: Service;
 
   /**
    * These are just used to create a working example
    * You should implement your own code to track the state of your accessory
    */
   private state = {
-    active: false,
+    on: false,
     mute: false,
     inputSource: 0,
+    isConfigured: false,
   };
 
   private id: string;
@@ -47,37 +48,46 @@ export class LmsModalPlayerAccessory {
     // this.service = this.accessory.getService(this.platform.Service.Speaker) || this.accessory.addService(this.platform.Service.Speaker);
     // this.service = this.accessory.getService(this.platform.Service.Outlet) || this.accessory.addService(this.platform.Service.Outlet);
 
-    this.inputService = this.accessory.getService(this.platform.Service.InputSource)
-      || this.accessory.addService(this.platform.Service.InputSource);
+    this.inputServices.push(this.accessory.getService(this.platform.Service.InputSource)
+      || this.accessory.addService(this.platform.Service.InputSource));
 
-    this.speakerService = this.accessory.getService(this.platform.Service.Speaker)
-      || this.accessory.addService(this.platform.Service.Speaker);
+    this.switchService = this.accessory.getService(this.platform.Service.Switch)
+      || this.accessory.addService(this.platform.Service.Switch);
 
-    // set accessory information
-    // this.accessory.getService(this.platform.Service.AccessoryInformation)!
-    //   .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Logitech')
-    //   .setCharacteristic(this.platform.Characteristic.Model, 'Squeezebox')
-    //   .setCharacteristic(this.platform.Characteristic.SerialNumber, this.id);
-
-    this.inputService.setCharacteristic(this.platform.Characteristic.ConfiguredName, this.playerName);
-    this.inputService.setCharacteristic(this.platform.Characteristic.Name, this.playerName);
-    this.inputService.setCharacteristic(this.platform.Characteristic.IsConfigured, true);
-    this.inputService.setCharacteristic(this.platform.Characteristic.CurrentVisibilityState, true);
-    this.inputService.setCharacteristic(this.platform.Characteristic.InputDeviceType,
-      this.platform.Characteristic.InputDeviceType.AUDIO_SYSTEM);
-
-    this.inputService.setCharacteristic(this.platform.Characteristic.InputSourceType, 0);
-    this.inputService.getCharacteristic(this.platform.Characteristic.InputSourceType)
-      .onSet(this.setInputSource.bind(this))
-      .onGet(this.getInputSource.bind(this));
-
-    this.speakerService.getCharacteristic(this.platform.Characteristic.Active)
+    this.switchService.getCharacteristic(this.platform.Characteristic.On)
       .onSet(this.setOn.bind(this))
       .onGet(this.getOn.bind(this));
 
-    this.speakerService.getCharacteristic(this.platform.Characteristic.Mute)
-      .onSet(this.setMute.bind(this))
-      .onGet(this.getMute.bind(this));
+    // set accessory information
+    this.accessory.getService(this.platform.Service.AccessoryInformation)!
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Logitech')
+      .setCharacteristic(this.platform.Characteristic.Model, 'Squeezebox')
+      .setCharacteristic(this.platform.Characteristic.SerialNumber, this.id);
+
+    this.inputServices[0].setCharacteristic(this.platform.Characteristic.Name, 'Mixer');
+
+    this.inputServices[0].setCharacteristic(this.platform.Characteristic.InputSourceType, 0);
+    this.inputServices[0].getCharacteristic(this.platform.Characteristic.InputSourceType)
+      .onSet(this.setInputSource.bind(this))
+      .onGet(this.getInputSource.bind(this));
+
+    // create handlers for required characteristics
+    this.inputServices[0].getCharacteristic(this.platform.Characteristic.ConfiguredName)
+      .onGet(this.handleConfiguredNameGet.bind(this))
+      .onSet(this.handleConfiguredNameSet.bind(this));
+
+    this.inputServices[0].getCharacteristic(this.platform.Characteristic.InputSourceType)
+      .onGet(this.handleInputSourceTypeGet.bind(this));
+
+    this.inputServices[0].getCharacteristic(this.platform.Characteristic.IsConfigured)
+      .onGet(this.handleIsConfiguredGet.bind(this))
+      .onSet(this.handleIsConfiguredSet.bind(this));
+
+    this.inputServices[0].getCharacteristic(this.platform.Characteristic.Name)
+      .onGet(this.handleNameGet.bind(this));
+
+    this.inputServices[0].getCharacteristic(this.platform.Characteristic.CurrentVisibilityState)
+      .onGet(this.handleCurrentVisibilityStateGet.bind(this));
   }
 
   /**
@@ -88,7 +98,7 @@ export class LmsModalPlayerAccessory {
 
     // const prevDeviceState = Boolean(this.state.deviceOn);
     const prevDeviceState = Boolean(this.platform.players[this.playerId].power);
-    this.state.active = value as boolean;
+    this.state.on = value as boolean;
 
     const client = new SlimServer(this.server);
     const power = await client.query(this.playerId, 'power', `${Number(value)}`);
@@ -123,25 +133,16 @@ export class LmsModalPlayerAccessory {
     const status = await client.query(this.playerId, 'status');
     this.platform.players[this.playerId].power = Number(status.power);
     // this.state.deviceOn = Boolean(Number(status.power));
-    this.state.active = this.platform.players[this.playerId].power;
+    this.state.on = this.platform.players[this.playerId].power;
 
     this.platform.log.debug('Power state', this.platform.players[this.playerId].power);
     this.platform.log.debug('Input states', (this.platform.players[this.playerId].input === this.input));
     // this.platform.log.debug('Power state', Boolean(Number(status.power)));
     // this.platform.log.debug('Input states', (this.platform.inputStates[this.playerId] === this.input));
-    this.platform.log.debug('Get Characteristic On ->', this.state.active);
+    this.platform.log.debug('Get Characteristic On ->', this.state.on);
     // if you need to return an error to show the device as "Not Responding" in the Home app:
     // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
-    return this.state.active;
-  }
-
-  async setMute(value: CharacteristicValue) {
-    this.platform.log.debug(`Set Characteristic Mute From ${value}`);
-  }
-
-  async getMute(): Promise<CharacteristicValue> {
-    this.platform.log.debug(`Get Characteristic Mute -> ${this.state.mute}`);
-    return this.state.mute;
+    return this.state.on;
   }
 
   async setInputSource(value: CharacteristicValue) {
@@ -151,6 +152,83 @@ export class LmsModalPlayerAccessory {
   async getInputSource(): Promise<CharacteristicValue> {
     this.platform.log.debug(`Get Characteristic InputSource -> ${this.input}`);
     return this.state.inputSource;
+  }
+
+  /**
+   * Handle requests to get the current value of the "Configured Name" characteristic
+   */
+  handleConfiguredNameGet() {
+    this.platform.log.debug('Triggered GET ConfiguredName');
+
+    // set this to a valid value for ConfiguredName
+    const currentValue = 1;
+
+    return currentValue;
+  }
+
+  /**
+   * Handle requests to set the "Configured Name" characteristic
+   */
+  handleConfiguredNameSet(value) {
+    this.platform.log.debug(`Triggered SET ConfiguredName: ${value}`);
+  }
+
+  /**
+   * Handle requests to get the current value of the "Input Source Type" characteristic
+   */
+  handleInputSourceTypeGet() {
+    this.platform.log.debug('Triggered GET InputSourceType');
+
+    // set this to a valid value for InputSourceType
+    const currentValue = this.platform.Characteristic.InputSourceType.OTHER;
+
+    return currentValue;
+  }
+
+
+  /**
+   * Handle requests to get the current value of the "Is Configured" characteristic
+   */
+  handleIsConfiguredGet() {
+    this.platform.log.debug('Triggered GET IsConfigured');
+
+    // set this to a valid value for IsConfigured
+    // const currentValue = this.platform.Characteristic.IsConfigured.NOT_CONFIGURED;
+    const currentValue = this.platform.Characteristic.IsConfigured.CONFIGURED;
+
+    return currentValue;
+  }
+
+  /**
+   * Handle requests to set the "Is Configured" characteristic
+   */
+  handleIsConfiguredSet(value) {
+    this.platform.log.debug(`Triggered SET IsConfigured: ${value}`);
+    this.state.isConfigured = value as boolean;
+  }
+
+  /**
+   * Handle requests to get the current value of the "Name" characteristic
+   */
+  handleNameGet() {
+    this.platform.log.debug('Triggered GET Name');
+
+    // set this to a valid value for Name
+    const currentValue = 1;
+
+    return currentValue;
+  }
+
+  /**
+   * Handle requests to get the current value of the "Current Visibility State" characteristic
+   */
+  handleCurrentVisibilityStateGet() {
+    this.platform.log.debug('Triggered GET CurrentVisibilityState');
+
+    // set this to a valid value for CurrentVisibilityState
+    const currentValue = this.platform.Characteristic.CurrentVisibilityState.SHOWN;
+
+    return currentValue;
   }
 
   sleep(ms: number): Promise<void> {
